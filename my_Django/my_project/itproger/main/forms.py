d160eac99a29c80832f9d99eee147747614da97f
David-Hakobyan1 <84774130+David-Hakobyan1@users.noreply.github.com>
2021-06-27 17:15:06 +0300

    Delete my_Django/my_project directory

--- my_Django/my_project/itproger/main/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class CalcForm(forms.Form):
-    first_number = forms.CharField(label='First number:', max_length=100)
-    symbol = forms.CharField(label='Symbol:', max_length=100)
-    secound_number = forms.CharField(label='Secound number:', max_length=100)
